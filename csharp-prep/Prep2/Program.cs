1e46ff2e85106a96728cbb0967bda90c735a3cbc
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2024-11-09 19:45:16 -0300

    ok

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What it´s your grade porcentage? ");
+        string studentGrade = Console.ReadLine();
+
+        int x = int.Parse(studentGrade);
+        string grade = "";
+
+
+
+        if (x >= 90)
+        {
+            grade = "A";
+        }
+        else if (x >= 80)
+        {
+            grade = "B";
+        }
+        else if (x >= 70)
+        {
+            grade = "C";
+        }
+        else if (x >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Good luck on the next time!");
+        }
     }
 }
