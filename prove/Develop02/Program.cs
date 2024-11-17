070ba077a5621f21875d4fa0305adca9ee456cbe
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2024-11-16 21:01:43 -0300

    journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,3 +1,6 @@
+// to Exceeding Requirements I decided to create a password to be able to open the entries, many people are afraid of writing a diary and someone reading something they shouldn't 
+// and the password helps protect that, to create the password I had to research a little more than just the week's material to arrive in the result.
+
 using System;
 using System.IO;
 
@@ -7,7 +10,23 @@ class Program
     {
         Journal journal = new Journal();
 
+        int password = 0;
+        string passwordFilePath = "password.txt";
+
+        if (File.Exists(passwordFilePath))
+        {
+            string savedPassword = File.ReadAllText(passwordFilePath);
+            if (int.TryParse(savedPassword, out int savedPasswordInt))
+            {
+                password = savedPasswordInt;
+            }
+        }
+
+
+
+
         Console.WriteLine("Hello! Please select one of the options below:");
+        Console.WriteLine("0. Password");
         Console.WriteLine("1. Write");
         Console.WriteLine("2. Display");
         Console.WriteLine("3. Load");
@@ -22,6 +41,32 @@ class Program
         {
             Console.Write("What would you like to do?");
             choice = int.Parse(Console.ReadLine());
+
+            if (choice == 0)
+            {
+                Console.Write("Please set a password (numbers only): ");
+                password = int.Parse(Console.ReadLine());
+                File.WriteAllText(passwordFilePath, password.ToString());
+                Console.WriteLine("Password has been set successfully!");
+            }
+            else if (choice == 2 || choice == 3 || choice == 4)
+            {
+                if (password == 0)
+                {
+                    Console.WriteLine("No password is set. Please set a password first.");
+                    continue;
+                }
+                Console.Write("Enter password: ");
+                int inputPassword = int.Parse(Console.ReadLine());
+
+                if (inputPassword != password)
+                {
+                    Console.WriteLine("Wrong password.");
+                    continue;
+
+                }
+
+            }
 
             if (choice == 1)
             {
