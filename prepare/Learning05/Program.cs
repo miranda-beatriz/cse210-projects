7ba4da896e451ae048b596d376efddbd18dd576f
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2024-12-05 13:31:41 -0300

    learning activity week 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment al = new Assignment("Bonnie Gilbert", "Multiplication");
+        Console.WriteLine(al.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Taylor Bennet", "Fraction", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssigment a3 = new WritingAssigment("Mary Walter", "European History", "The Causes of World War 2");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
