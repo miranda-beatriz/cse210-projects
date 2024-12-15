f09b3750b8a1799e54f9b3b13c3c1ad3fb65825f
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2024-12-15 20:20:28 -0300

    foudantion 3

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -7,9 +7,9 @@ namespace FitnessCenter
         {
             var activities = new List<Activity>
             {
-                new Running(new DateTime(2024, 13, 12), 30, 5),
-                new Cycling(new DateTime(2024, 05, 12), 60, 20),
-                new Swimming(new DateTime(2024, 23, 12), 45, 40)
+                new Running(new DateTime(2024, 12, 13), 30, 5),
+                new Cycling(new DateTime(2024, 12, 05), 60, 20),
+                new Swimming(new DateTime(2024, 12, 23), 45, 40)
             };
 
             foreach (var activity in activities)
